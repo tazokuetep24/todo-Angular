c17da1ee423c75630774c091e31c9742638c6f01
Patrick Tazo Kuete <117319444+tazokuetep24@users.noreply.github.com>
2025-02-17 20:56:14 +0100

    styling and add Item functionality added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,41 @@
 import { Component } from '@angular/core';
+import { CommonModule} from "@angular/common";
 
 @Component({
+  standalone: true,
   selector: 'app-root',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'todo';
+  componentTitle = 'My To Do List';
+
+  filter: "all" | "active" | "done" = "all";
+
+  allItems = [
+{description: "eat", done: true},
+{description: "sleep", done: false},
+{description: "play", done: false},
+{description: "laugh", done: false},
+
+  ];
+
+  addItem(description: string){
+
+    if(!description) return;
+    this.allItems.unshift({
+      description,
+      done: false
+    });
+  }
+
+  get items(){
+    if(this.filter === "all"){
+      return this.allItems;
+    }
+    return this.allItems.filter((item) => 
+      this.filter === "done" ? item.done : !item.done
+  );
+  }
 }
